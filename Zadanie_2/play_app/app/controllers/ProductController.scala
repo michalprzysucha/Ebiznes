f8b307891803a8ada420b355515ded4708becc62
michalprzysucha <mpbeliar@gmail.com>
2023-03-28 13:38:20 +0200

    delete and update implementation

--- Zadanie_2/play_app/app/controllers/ProductController.scala
+++ Zadanie_2/play_app/app/controllers/ProductController.scala
@@ -5,7 +5,6 @@ import play.api.mvc._
 import scala.collection.mutable.ListBuffer
 import play.api.libs.json._
 import models.Product
-//import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ProductController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
@@ -31,6 +30,17 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
     }
   }
 
+  def deleteProduct(id: Long) = Action {
+    val productIndex = productsList.indexWhere(_.id == id)
+    if (productIndex != -1){
+      val product = productsList(productIndex)
+      productsList.remove(productIndex)
+      Ok(s"Produkt ${product.name} o id ${product.id} został usunięty")
+    } else {
+      NotFound
+    }
+  }
+
   def addProduct(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val body = request.body
     val jsonBodyOption: Option[JsValue] = body.asJson
@@ -46,6 +56,29 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
         Ok("Produkt dodany pomyślnie.")
       }
       case None => BadRequest("Oczekiwano ciała z danymi JSON.")
+    }
+  }
+
+  def updateProduct(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val productIndex = productsList.indexWhere(_.id == id)
+    if (productIndex != -1) {
+      val product = productsList(productIndex)
+      val body = request.body
+      val jsonBodyOption: Option[JsValue] = body.asJson
+
+      jsonBodyOption match {
+        case Some(jsonBody) => {
+          val name = (jsonBody \ "name").as[String]
+          val description = (jsonBody \ "description").as[String]
+          val price = (jsonBody \ "price").as[Double]
+
+          productsList(productIndex) = Product(product.id, name, description, price)
+          Ok(s"Produkt ${product.name} o id ${product.id} został poprawnie zmodyfikowany")
+        }
+        case None => BadRequest("Oczekiwano ciała z danymi JSON.")
+      }
+    } else {
+      NotFound
     }
   }
 }
